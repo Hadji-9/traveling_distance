416e482f4bddb1d17c185dcf979a3d4e6c52d310
khalil <khalilhad9@gmail.com>
2022-11-10 16:20:13 +0100

    added sync implementation just in case

--- main_sync.py
+++ main_sync.py
@@ -1,15 +1,14 @@
 import httpx
 import time
 from app.gen_coordinates import get_city_coords
-from app.gen_driving_distances import get_driving_distance
+from app.gen_driving_distances import get_driving_distance_sync
 from app.read_write_data import read_city_file, write_coordinates, read_coordinates, write_distance_matrix
-import asyncio
-sem = asyncio.Semaphore(4)
+import typer
+cities_coordinates = {"cities": []}
 
 
 def run_coordinates():
     cities_names = read_city_file()
-    cities_coordinates = {"cities": []}
 
     result = []
     start = time.time()
@@ -31,41 +30,29 @@ def run_coordinates():
     write_coordinates(coordinates=cities_coordinates)
 
 
-async def insert_distance(client: httpx.AsyncClient(), i, j, city_1, city_2, city_1_name, city_2_name, distance_matrix):
-    async with sem:
-        print(f" combinaison : {i} --- {j} ")
-
-        if i < j:
-            distance = await get_driving_distance(
-                client=client, city_coordinate_1=city_1.get("coordinates"), city_coordinate_2=city_2.get("coordinates"))
-        elif i > j:
-            distance = None
-        else:
-            distance = 0
-        if sem.locked():
-            print("Concurrency limit reached, waiting ...")
-            await asyncio.sleep(1)
-        distance_matrix[city_1_name][city_2_name] = distance
-
-
-async def run_driving_distances():
-    tasks = []
+def run_driving_distances():
     distance_matrix = {}
     cities: list[dict] = read_coordinates()
-    async with httpx.AsyncClient() as client:
+    with httpx.Client() as client:
         for i, city_1 in enumerate(cities):
+            start = time.time()
             city_1_name = city_1.get("city")
             distance_matrix[city_1_name] = {}
             for j, city_2 in enumerate(cities):
                 city_2_name = city_2.get("city")
-                tasks.append(insert_distance(client=client, i=i, j=j,
-                                             city_1=city_1, city_2=city_2, city_1_name=city_1_name, city_2_name=city_2_name, distance_matrix=distance_matrix))
-        await asyncio.gather(*tasks)
+
+                if i < j:
+                    distance = get_driving_distance_sync(
+                        client=client, city_coordinate_1=city_1.get("coordinates"), city_coordinate_2=city_2.get("coordinates"))
+                elif i > j:
+                    distance = None
+                else:
+                    distance = 0
+
+                distance_matrix[city_1_name][city_2_name] = distance
+            print(f" 185 distance took {time.time()-start} seconds")
     write_distance_matrix(distance_matrix=distance_matrix)
 
 
 if __name__ == "__main__":
-    # uncomment to get cities coordinates
-    # run_coordinates()
-
-    asyncio.run(run_driving_distances())
+    run_driving_distances()
